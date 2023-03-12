302ed3f254979516ed5b8c5b67890aa1a7a1ecb7
bicdan22 <bicdan22@yandex-team.ru>
2023-03-12 21:58:36 +0300

    hw5

---
+++ 05_DiffPatchNet/chat.py
@@ -0,0 +1,33 @@
+import asyncio
+import cowsay
+
+clients = {}
+
+async def chat(reader, writer):
+    me = "{}:{}".format(*writer.get_extra_info("peername"))
+    print(me)
+    clients[me] = asyncio.Queue()
+    send = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(clients[me].get())
+
+    while not reader.at_eof():
+        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        for q in done:
+            if q is send:
+                send = asyncio.create_task(reader.readline())
+                for out in clients.values():
+                    if out is not clients[me]:
+                        await out.put(f"{me} {q.result().decode().strip()}")
+            elif q is receive:
+                receive = asyncio.create_task(clients[me].get())
+                writer.write(f"{q.result()}\n".encode())
+                await writer.drain()
+    send.cancel()
+    receive.cancel()
+
+async def main():
+    server = await asyncio.start_server(chat, "0.0.0.0", 1234)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
